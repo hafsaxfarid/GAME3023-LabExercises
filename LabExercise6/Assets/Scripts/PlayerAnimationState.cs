6950f389cf7604b61407ff32d731deb7722bdf9c
hafsaxfarid <hafsaxfarid@gmail.com>
2022-11-10 16:36:51 -0500

    Lab 6 Files Added

---
+++ LabExercise6/Assets/Scripts/PlayerAnimationState.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerAnimationState
+{
+    IDLE,
+    WALK_UP,
+    WALK_DOWN,
+    WALK_LEFT,
+    WALK_RIGHT,
+    NUM_OF_ANIMATION_STATES
+}
