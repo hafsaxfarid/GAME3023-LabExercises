af435036c1c5b3a84001e49e3cd55ada71850abf
hafsaxfarid <hafsaxfarid@gmail.com>
2022-12-08 18:59:25 -0500

    Lab 10 - Creating Crafting Menu
    Added a button to toggle crafting options. WIP on scripting functionality for crafting items

---
+++ LabExercise6/Assets/Scripts/CraftingManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftingManager : MonoBehaviour
+{
+    [SerializeField]
+    private Item currentItem;
+
+    public void OnMouseDownItem(Item item)
+    {
+        
+    }
+}
